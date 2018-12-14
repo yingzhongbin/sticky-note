11e20a44773fe1b5b353ae739c2d71e82e6f0827
yingzhongbin <1067339571@qq.com>
2018-12-14 14:09:28 +0800

    修改cursor

--- routes/auth.js
+++ routes/auth.js
@@ -15,10 +15,12 @@ passport.deserializeUser(function(obj, done) {
 });
 
 passport.use(new GitHubStrategy({
-      clientID: 'f1171d4773dcf679738f',
-      clientSecret: '58fbf8119b3b6e78349531f8313e2bab8ad07ffa',
-      callbackURL: "http://127.0.0.1:2988/auth/github/callback",
+      // clientID: 'f1171d4773dcf679738f',
+      // clientSecret: '58fbf8119b3b6e78349531f8313e2bab8ad07ffa',
       // callbackURL: "http://yingzhongbin.top/auth/github/callback"
+      clientID: '6d8965aea7ff255e1c50',
+      clientSecret: '623b3daceb36abd1bba0fb500c2bf93727229f4a',
+      callbackURL: "http://127.0.0.1:2988/auth/github/callback"
     },
     function(accessToken, refreshToken, profile, done) {
       // User.findOrCreate({ githubId: profile.id }, function (err, user) {
